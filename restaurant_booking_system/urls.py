6ac4c5cf574131415bd8b3ce7e5c86928b679dc0
Jin Nordén <rvphogir@students.codeinstitute.net>
2024-11-27 18:20:56 +0000

    moved base and login html

--- restaurant_booking_system/urls.py
+++ restaurant_booking_system/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('bookings.urls')),  # URLs from the bookings app
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
