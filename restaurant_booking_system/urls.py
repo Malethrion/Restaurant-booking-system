400d9f412926f2bb40dd17a15a59228933fee9be
Jin Nordén <rvphogir@students.codeinstitute.net>
2024-11-27 13:41:41 +0000

    changed name to restaurant_booking_system

--- restaurant_booking_system/urls.py
+++ restaurant_booking_system/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for restaurant_booking project.
+URL configuration for restaurant_booking_system project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
