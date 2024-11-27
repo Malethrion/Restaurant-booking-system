f98480ce08b7d69bcafb39c19b2c939ad3d4a649
Jin Nordén <rvphogir@students.codeinstitute.net>
2024-11-27 10:53:50 +0000

    Add simple reservetation features and FAQ to bookings models

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Reservations
+class Reservation(models.Model):
+    customer_name = models.CharField(max_length=100)  # Name of the person making the reservation
+    date = models.DateField()  # Date of the reservation
+    time = models.TimeField()  # Time of the reservation
+    guests = models.IntegerField()  # Number of guests
+    contact_email = models.EmailField()  # Contact email for the reservation
+
+# FAQ
+class FAQ(models.Model):
+    question = models.CharField(max_length=200)  # Question text
+    answer = models.TextField()  # Answer text
