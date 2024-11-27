73578f5cf8afee46f390a3748aeb0b20d084fbdc
Jin Nordén <rvphogir@students.codeinstitute.net>
2024-11-27 15:02:14 +0000

    third try to commit changes to views for reservation

--- bookings/views.py
+++ bookings/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import ReservationForm
+from .models import Reservation
+
+
+def create_reservation(request):
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list_reservations')  # Redirect to a list of reservations
+    else:
+        form = ReservationForm()
+    return render(request, 'bookings/create_reservation.html', {'form': form})
+
 
 def index(request):
     return render(request, 'bookings/index.html')  # Render the index.html template
-    
+
+
 def register(request):
     if request.method == 'POST':  # If the form is submitted
         form = UserRegistrationForm(request.POST)
