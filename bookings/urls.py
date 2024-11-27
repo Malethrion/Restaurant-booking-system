a15c5aa5078e8026adbb5eb297c4480e645565ba
Jin Nordén <rvphogir@students.codeinstitute.net>
2024-11-27 15:37:09 +0000

    add empty reservations html files, add url paths

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),  # Root URL for the bookings app
     path('register/', views.register, name='register'),  # Registration URL
+    path('reservations/', views.list_reservations, name='list_reservations'),  # Reservations list
+    path('reservations/new/', views.create_reservation, name='create_reservation'),  # Create reservation
+    path('reservations/<int:pk>/edit/', views.update_reservation, name='update_reservation'),  # Update reservation
+    path('reservations/<int:pk>/delete/', views.delete_reservation, name='delete_reservation'),  # Delete reservation
 ]
