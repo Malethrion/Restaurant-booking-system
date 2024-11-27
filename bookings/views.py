489f7bfab2ff35c6f05e949631d81a17dd6d6f01
Jin Nordén <rvphogir@students.codeinstitute.net>
2024-11-27 12:42:11 +0000

    add registration view handling sibmitted posts and requests

--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import UserRegistrationForm  # Importing form
 
-# Create your views here.
-def index(request):
-    return HttpResponse("bookings")
+def register(request):
+    if request.method == 'POST':  # If the form is submitted
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():  # Check if the form is valid
+            form.save()  # Save the user to the database
+            return redirect('login')  # Redirect the user to the login page
+    else:
+        form = UserRegistrationForm()  # Display an empty form
+    return render(request, 'register.html', {'form': form})  # Render the template
