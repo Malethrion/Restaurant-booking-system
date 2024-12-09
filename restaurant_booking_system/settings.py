34038a1841e8459819bd4bb941c8d0ec75bde9d2
Jin Nordén <rvphogir@students.codeinstitute.net>
2024-12-09 05:14:24 +0000

    Collect static files + updated settings

--- restaurant_booking_system/settings.py
+++ restaurant_booking_system/settings.py
@@ -119,6 +119,9 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
